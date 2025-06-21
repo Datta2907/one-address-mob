4f04d65140093b00ec0edf6e6389125673ae791d
srinivas datta <datta598764@gmail.com>
2025-06-20 21:27:38 -0600

    changes for mac

--- screens/signInOrSignUp.js
+++ screens/signInOrSignUp.js
@@ -45,7 +45,7 @@ export function SignInOrSignUpComponent({ navigation }) {
 
     useEffect(() => {
         GoogleSignin.configure({
-            webClientId: process.env.EXPO_PUBLIC_WEB_CLIENT_ID,
+            webClientId: "821851695738-u2035r45nhfncldhdmm7kkf6g8un9k0t.apps.googleusercontent.com",
             offlineAccess: true,
         })
         getCommunities();
--- utils/environment_variables.js
+++ utils/environment_variables.js
@@ -1,5 +1,5 @@
 const environment = {
-    api: 'http://192.168.0.100:5000/'
+    api: 'http://10.0.0.228:6000/'
 }
 
 export default environment;
